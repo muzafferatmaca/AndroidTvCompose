647a18b5ec5d8a0de68cbb9a9542b5c71cb28d28
muzaffer-atmaca <muzafferatmaca@gmail.com>
2024-06-08 17:58:11 +0300

    - Category Browser - Details

--- app/src/main/java/com/muzafferatmaca/androidtvcompose/presentation/screens/catalog/CatalogBrowser.kt
+++ app/src/main/java/com/muzafferatmaca/androidtvcompose/presentation/screens/catalog/CatalogBrowser.kt
@@ -1,8 +1,19 @@
 package com.muzafferatmaca.androidtvcompose.presentation.screens.catalog
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.tv.foundation.lazy.list.TvLazyColumn
+import androidx.tv.foundation.lazy.list.TvLazyRow
+import androidx.tv.foundation.lazy.list.items
+import com.muzafferatmaca.androidtvcompose.domain.entity.Movie
+import com.muzafferatmaca.androidtvcompose.presentation.components.MovieCard
 
 /**
  * Created by Muzaffer Atmaca on 8.06.2024 at 00:35
@@ -13,4 +24,20 @@ fun CatalogBrowser(
     catalogBrowserViewModel: CatalogBrowserViewModel = hiltViewModel(),
     onMovieSelected: (Movie) -> Unit = {}
 ) {
+    val categoryList by catalogBrowserViewModel.categoryList.collectAsStateWithLifecycle()
+
+    TvLazyColumn(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 48.dp, vertical = 32.dp)
+    ) {
+        items(categoryList) { category ->
+            Text(text = category.name)
+            TvLazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                items(category.movieList) {
+                    MovieCard(movie = it, onClick = { onMovieSelected(it) })
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/muzafferatmaca/androidtvcompose/presentation/screens/details/Details.kt
+++ app/src/main/java/com/muzafferatmaca/androidtvcompose/presentation/screens/details/Details.kt
@@ -1,7 +1,21 @@
 package com.muzafferatmaca.androidtvcompose.presentation.screens.details
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.muzafferatmaca.androidtvcompose.domain.entity.Movie
 
 /**
@@ -9,4 +23,44 @@ import com.muzafferatmaca.androidtvcompose.domain.entity.Movie
  */
 @Composable
 fun Details(movie: Movie, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .padding(
+                vertical = 32.dp,
+                horizontal = 48.dp
+            )
+    ) {
+        Box(modifier = modifier.fillMaxSize()) {
+            AsyncImage(
+                model = movie.cardImageUrl,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+            Box(
+                modifier = Modifier
+                    .background(
+                        Brush.linearGradient(
+                            listOf(
+                                MaterialTheme.colorScheme.background,
+                                Color.Transparent
+                            )
+                        )
+                    )
+                    .fillMaxSize()
+            ){
+                Column(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(horizontal = 48.dp, vertical = 24.dp)
+                ) {
+                    Text(text = movie.title, style = MaterialTheme.typography.displayMedium)
+                    Text(text = movie.studio, style = MaterialTheme.typography.bodySmall)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = movie.description)
+                }
+            }
+
+        }
+    }
 }
